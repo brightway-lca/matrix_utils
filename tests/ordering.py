03614c40fe76531433c524707ef884b796246f77
Chris Mutel <cmutel@gmail.com>
2021-11-24 17:22:49 +0100

    Test ordering of resources

---
+++ tests/ordering.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from matrix_utils import MappedMatrix
+from fs.zipfs import ZipFS
+import numpy as np
+
+from bw_processing import load_datapackage
+
+dirpath = Path(__file__).parent.resolve() / "fixtures"
+
+
+class Interface:
+    def __next__(self):
+        return np.arange(3)
+
+
+def test_ordering():
+    dps = [load_datapackage(ZipFS(dirpath / "b-second.zip")), load_datapackage(ZipFS(dirpath / "a-first.zip"))]
+    for dp in dps:
+        dp.rehydrate_interface("w-fourth", Interface())
+        print(list(dp.groups))
+
+    mm = MappedMatrix(packages=dps, matrix="matrix-a")
+    assert [grp.label for grp in mm.groups] == ['y-second', 'w-fourth', 'y-second', 'w-fourth']
