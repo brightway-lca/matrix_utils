e920600f9130f39a133cb0fe5526da48d0133e13
Chris Mutel <cmutel@gmail.com>
2021-11-25 13:52:26 +0100

    Add more test fixtures

--- tests/fixtures/create_static_fixtures.py
+++ tests/fixtures/create_static_fixtures.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
-from fs.osfs import OSFS
 from fs.zipfs import ZipFS
 
 from bw_processing import INDICES_DTYPE, create_datapackage, UNCERTAINTY_DTYPE
@@ -41,6 +39,9 @@ w-fourth
 
 This file creates two datapackages. The values in `b-second` will be the same as in  `a-first`, but incremented by 100.
 """
+
+
+dirpath = Path(__file__).parent.resolve()
 
 
 class Dummy:
@@ -85,10 +86,6 @@ def add_data(dp, increment=0):
 
 
 def create_ordering_datapackages():
-    # Create the test fixtures
-
-    dirpath = Path(__file__).parent.resolve()
-
     dp = create_datapackage(
         fs=ZipFS(str(dirpath / "a-first.zip"), write=True),
         name="test-fixture-a",
@@ -106,5 +103,86 @@ def create_ordering_datapackages():
     dp.finalize_serialization()
 
 
+def create_sensitivity_fixtures():
+    dp = create_datapackage(
+        fs=ZipFS(str(dirpath / "sa-1.zip"), write=True),
+        name="sa-1",
+        id_="sa-1",
+        seed=42,
+    )
+
+    # x 0 0
+    # 0 0 x
+    # 0 x 0
+    # where x is the column chosen
+
+    indices_array = np.array([(10, 10), (11, 12), (12, 11)], dtype=INDICES_DTYPE)
+    dp.add_dynamic_array(
+        interface=Dummy(),
+        matrix="matrix",
+        name="a",
+        indices_array=indices_array,
+    )
+
+    # -x  0     0
+    # 0  x + 1 0
+    # 0  0     0
+    # where x is column plus 7
+
+    indices_array = np.array([(10, 10), (11, 11)], dtype=INDICES_DTYPE)
+    data_array = np.array([[7, 8], [8, 9], [9, 10], [10, 11]]).T
+    flip_array = np.array([1, 0], dtype=bool)
+    dp.add_persistent_array(
+        matrix="matrix",
+        data_array=data_array,
+        name="b",
+        indices_array=indices_array,
+        flip_array=flip_array,
+    )
+
+    dp.finalize_serialization()
+
+    dp = create_datapackage(
+        fs=ZipFS(str(dirpath / "sa-2.zip"), write=True),
+        name="sa-2",
+        id_="sa-2",
+    )
+
+    # 0 0   0
+    # 0 0   0
+    # 1 2-1 -3
+
+    data_array = np.array([1, 2, 1, 3])
+    flip_array = np.array([0, 0, 1, 1], dtype=bool)
+    indices_array = np.array([(12, 10), (12, 11), (12, 11), (12, 12)], dtype=INDICES_DTYPE)
+    distributions_array = np.zeros((4,), dtype=UNCERTAINTY_DTYPE)
+    distributions_array['uncertainty_type'] = (4, 4, 0, 4)
+    distributions_array['minimum'] = (0.5, 1.5, np.NaN, 2.5)
+    distributions_array['maximum'] = (1.5, 2.5, np.NaN, 3.5)
+    dp.add_persistent_vector(
+        matrix="matrix",
+        data_array=data_array,
+        name="c",
+        indices_array=indices_array,
+        flip_array=flip_array,
+        distributions_array=distributions_array,
+    )
+
+    # 0 0 1
+    # 0 0 0
+    # 2 0 3
+
+    indices_array = np.array([(10, 12), (12, 10), (12, 12)], dtype=INDICES_DTYPE)
+    dp.add_dynamic_vector(
+        interface=Dummy(),
+        indices_array=indices_array,
+        matrix="matrix",
+        name="d",
+    )
+
+    dp.finalize_serialization()
+
+
 if __name__ == "__main__":
     create_ordering_datapackages()
+    create_sensitivity_fixtures()
