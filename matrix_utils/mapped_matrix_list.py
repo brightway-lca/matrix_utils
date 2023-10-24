11573b49642f624a0fad2e40d2d46b1eb3fa45a6
Chris Mutel <cmutel@gmail.com>
2023-10-24 22:41:42 +0200

    Switch from list to dict

--- matrix_utils/mapped_matrix_list.py
+++ matrix_utils/mapped_matrix_list.py
@@ -1,14 +1,23 @@
+from collections.abc import Mapping
+from typing import Any, Callable, Optional, Sequence, Union
+
+from bw_processing import Datapackage
+
+from .array_mapper import ArrayMapper
 from .mapped_matrix import MappedMatrix
 
 
-class MappedMatrixList:
+class MappedMatrixDict(Mapping):
     """Class which handles a list of mapped matrices.
 
-    The matrices have the same dimensions, the same lookup dictionaries, and the same indexer."""
+    The matrices have the same dimensions, the same lookup dictionaries, and the same
+    indexer.
+    """
+
     def __init__(
         self,
         *,
-        packages: dict[str, Sequence[Datapackage]],
+        packages: dict[Union[tuple, str], Sequence[Datapackage]],
         matrix: str,
         row_mapper: ArrayMapper,
         col_mapper: ArrayMapper,
@@ -22,12 +31,14 @@ class MappedMatrixList:
         custom_filter: Optional[Callable] = None,
         empty_ok: bool = False,
     ):
-        """A thin wrapper around a list of `MappedMatrix` objects. See its docstring for details on `custom_filter` and `indexer_override`.
+        """A thin wrapper around a list of `MappedMatrix` objects. See its docstring
+        for details on `custom_filter` and `indexer_override`.
 
         Parameters
         ----------
-        packages : list[Datapackage]
-            A list of Datapackage objects.
+        packages : dict[Union[tuple, str], Sequence[Datapackage]]
+            A dictionary with identifiers as keys and a list of `bw_processing`
+            datapackages as values.
         matrix : str
             The string identifying the matrix to be built.
         use_vectors : bool
@@ -47,10 +58,42 @@ class MappedMatrixList:
         diagonal : bool
             If `True`, only use the `row` indices to build a diagonal matrix.
         transpose : bool
-            Transpose row and column indices. Happens before any processing, so filters and mappers should refer to the transposed dimensions.
+            Transpose row and column indices. Happens before any processing, so filters
+            and mappers should refer to the transposed dimensions.
         custom_filter : Callable
             Callable for function to filter data based on `indices` values. See above.
         empty_ok : bool
             If False, raise `AllArraysEmpty` if the matrix would be empty
 
         """
+        self.matrices = {
+            tpl: MappedMatrix(
+                packages=packages,
+                matrix=matrix,
+                use_vectors=use_vectors,
+                use_arrays=use_arrays,
+                use_distributions=use_distributions,
+                row_mapper=row_mapper,
+                col_mapper=col_mapper,
+                seed_override=seed_override,
+                indexer_override=None,
+                diagonal=diagonal,
+                transpose=transpose,
+                custom_filter=custom_filter,
+                empty_ok=empty_ok,
+            )
+            for tpl, packages in packages.items()
+        }
+
+    def __getitem__(self, key: Union[tuple, str]) -> MappedMatrix:
+        return self.matrices[key]
+
+    def __iter__(self):
+        for tpl in self.matrices:
+            return self.matrices[tpl]
+
+    def __len__(self) -> int:
+        return len(self.matrices)
+
+    def __next__(self) -> None:
+        pass
