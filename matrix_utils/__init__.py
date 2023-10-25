23fdaf7af92f43658a8882f05df73dc089e9e812
Chris Mutel <cmutel@gmail.com>
2023-10-25 22:10:14 +0200

    0.3.dev1

--- matrix_utils/__init__.py
+++ matrix_utils/__init__.py
@@ -10,7 +10,7 @@ __all__ = (
     "Proxy",
 )
 
-__version__ = "0.3"
+__version__ = "0.3.dev1"
 
 from .array_mapper import ArrayMapper
 from .indexers import CombinatorialIndexer, Proxy, RandomIndexer, SequentialIndexer
